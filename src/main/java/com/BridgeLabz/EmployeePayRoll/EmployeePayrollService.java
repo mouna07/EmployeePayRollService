3fba1a04acd7b5d1f9eb374a6df21ca7d1129c77
mouna07 <mounamc261@gmail.com>
2021-03-04 08:40:21 +0530

    ADDED: welcome statement

---
+++ src/main/java/com/BridgeLabz/EmployeePayRoll/EmployeePayrollService.java
@@ -0,0 +1,8 @@
+package com.BridgeLabz.EmployeePayRoll;
+import java.util.Scanner;
+
+public class EmployeePayrollService {
+    public static void main(String[] args) {
+        System.out.println("welcome to the employee pay roll service program");
+    }
+}
